c73063fbab287bd8f5286a0b0e09dce1ba7bb85b
Andy <1187820970@qq.com>
2016-02-16 15:04:33 +0800

    first init

---
+++ Assets/Forest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forest : MonoBehaviour {
+
+	private Transform player;
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag (Tags.player).transform;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (player.position.z > transform.position.z+100) {
+			Camera.main.SendMessage("GenrateForeat");
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Tags.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+	public const string player="Player";	
+}
