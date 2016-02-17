31dc96b85d3c1e552288b5fe5a58646a485872ef
Andy <1187820970@qq.com>
2016-02-18 00:08:29 +0800

    添加障碍物 游戏转态 角色移动等

---
+++ Assets/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState
+{
+    Menu,
+	Playing,
+	End
+}
+
+public class GameController : MonoBehaviour {
+
+	public static GameState gameState=GameState.Menu; 
+   
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	public float moveSpeed=100;
+	private EnvGenrator envGenerator;
+	void Awake()
+	{
+		envGenerator = Camera.main.GetComponent<EnvGenrator> ();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 targetPosition = envGenerator.forest1.GetTargetPoint();
+		Vector3 moveDir = targetPosition - transform.position;
+	    transform.position+=moveDir.normalized*moveSpeed*Time.deltaTime;
+		//transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime);
+	}
+}
