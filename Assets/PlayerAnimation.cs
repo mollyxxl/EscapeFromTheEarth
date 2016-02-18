376e0920e0cd123b0b2573ea8891a0caa68d0bc0
Andy <1187820970@qq.com>
2016-02-18 21:46:57 +0800

    获取鼠标滑动的状态

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum AnimationState
+{
+	Idle,
+	Run
+}
+public class PlayerAnimation : MonoBehaviour {
+
+	private Animation animation;
+	private AnimationState animationState=AnimationState.Idle;
+	void Awake()
+	{ 
+		animation = transform.Find ("Prisoner").animation;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (GameController.gameState == GameState.Menu) {
+					animationState = AnimationState.Idle;
+				} 
+		else if (GameController.gameState == GameState.Playing) {
+			animationState= AnimationState.Run;	
+		}
+	}
+	void LateUpdate()
+	{
+		switch (animationState) {
+		case AnimationState.Idle:
+			PlayIdle();
+			break;
+		case AnimationState.Run:
+			PlayAnimation("run");
+			break;
+		}
+	}
+	void PlayIdle()
+	{
+		if (animation.IsPlaying ("Idle_1") == false && animation.IsPlaying ("Idle_2") == false) {
+			animation.Play("Idle_1");
+			animation.PlayQueued("Idle_2");
+		}
+	}
+	void PlayAnimation(string animationName)
+	{
+		if(animation.IsPlaying(animationName)==false)
+		 {
+			animation.Play(animationName);
+		 }
+	}
+}
--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,26 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum TouchDirection
+{
+	None,
+	Left,
+	Right,
+	Top,
+	Bottom
+}
 public class PlayerMove : MonoBehaviour {
 	public float moveSpeed=100;
 	private EnvGenrator envGenerator;
+	private TouchDirection touchDirection=TouchDirection.None;
+	private Vector3 lastMouseDown=Vector3.zero;
 	void Awake()
 	{
 		envGenerator = Camera.main.GetComponent<EnvGenrator> ();
-	}
-	// Use this for initialization
-	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		if (GameController.gameState == GameState.Playing) {
-						Vector3 targetPosition = envGenerator.forest1.GetTargetPoint ();
-						Vector3 moveDir = targetPosition - transform.position;
-						transform.position += moveDir.normalized * moveSpeed * Time.deltaTime;
-						//transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime);
+			Vector3 targetPosition = envGenerator.forest1.GetTargetPoint ();
+			Vector3 moveDir = targetPosition - transform.position;
+			transform.position += moveDir.normalized * moveSpeed * Time.deltaTime;
+			//transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime);
+		     
+			MoveControl();
+		}
+	}
+	void MoveControl()
+	{
+		TouchDirection direction = GetTouchDirection ();
+		Debug.Log (direction);
+	}
+	TouchDirection GetTouchDirection()
+	{
+         if (Input.GetMouseButtonDown (0)) {
+			lastMouseDown=Input.mousePosition;		
+		 }
+		 if (Input.GetMouseButtonUp (0)) {
+			Vector3 mouseUp=Input.mousePosition;
+			Vector3 touchOffset=mouseUp-lastMouseDown;
+			float offset_x=Mathf.Abs(touchOffset.x);
+			float offset_y=Mathf.Abs(touchOffset.y);
+			if(offset_x>50||offset_y>50)
+			{
+				if(offset_x>offset_y&&touchOffset.x>0)
+				{
+					return TouchDirection.Right;
 				}
+				else if(offset_x>offset_y&&touchOffset.x<0)
+				{
+					return TouchDirection.Left;
+				}
+				else if(offset_x<offset_y&&touchOffset.y>0)
+				{
+					return TouchDirection.Top;
+				}
+				else if(offset_x<offset_y&&touchOffset.y<0)
+				{
+					return TouchDirection.Bottom;
+				}
+			}
+		  }
+		return TouchDirection.None;
 	}
 }
